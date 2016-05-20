8e614194098af9facf4f11f9739907ac7190ee9a
Ltven0mI <lt.ven0mi@gmail.com>
2016-05-20 18:49:38 +1000

    Added ImageUtil Class

---
+++ Pixel Studio/Pixel Studio/Utilities/ImageUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixel_Studio.Utilities
+{
+    public static class ImageUtil
+    {
+        public static Bitmap CreateBitmap(int width, int height, Color fillColor)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+                g.FillRectangle(new SolidBrush(fillColor), 0, 0, width, height);
+            return result;
+        }
+    }
+}
